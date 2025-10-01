f98644166d2e9d28cca9e3900cc45f81ea0b3a6e
Vitoria-Araujo <vitoria.araujo1@hotmail.com git config --global user.name Vitoria-Araujogit statusgit config --global user.email vitoria.araujo1@hotmail.comgit config --global user.email vitoria.araujo1@hotmail.com>
2025-10-01 11:40:44 -0300

    Declare duas variáveis e atribua valores numéricos a elas. Use o operador de adição para realizar operações entre as duas variáveis. Imprima o resultado.

---
+++ exercicio5.ts
@@ -0,0 +1,8 @@
+//Declare duas variáveis e atribua valores numéricos a elas. Use o operador de adição para realizar operações entre as duas variáveis. Imprima o resultado.
+
+let num6: number = 50;
+let num7: number = 20;
+
+let soma:number = num6 + num7;
+
+console.log(soma);
