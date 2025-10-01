445a6e8d8bf209aa6f140bb6ac4495bc8f682145
Vitoria-Araujo <vitoria.araujo1@hotmail.com git config --global user.name Vitoria-Araujogit statusgit config --global user.email vitoria.araujo1@hotmail.comgit config --global user.email vitoria.araujo1@hotmail.com>
2025-10-01 11:35:51 -0300

    Exercício 1: Declare duas variáveis chamadas idade e peso, atribua valores a elas e imprima os valores na console.

---
+++ exercicio1.ts
@@ -0,0 +1,5 @@
+//Exercício 1: Declare duas variáveis chamadas "idade" e "peso", atribua valores a elas e imprima os valores na console.
+let idade: number = 25;
+let peso: number = 32;
+
+console.log(`Seu peso é ${peso} e sua idade é ${idade}`);
