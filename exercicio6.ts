c24f62fa5b8e08d215dd220ea2a8895b80b7cc02
Vitoria-Araujo <vitoria.araujo1@hotmail.com git config --global user.name Vitoria-Araujogit statusgit config --global user.email vitoria.araujo1@hotmail.comgit config --global user.email vitoria.araujo1@hotmail.com>
2025-10-01 11:41:53 -0300

    Exercício 6: Declare duas variáveis e atribua valores booleanos a elas. Use os operadores lógicos AND, OR e NOT para realizar operações lógicas entre as variáveis.Imprima os resultados.

---
+++ exercicio6.ts
@@ -0,0 +1,9 @@
+//Exercício 6: Declare duas variáveis e atribua valores booleanos a elas.
+//  Use os operadores lógicos AND, OR e NOT para realizar operações lógicas entre as variáveis.
+// Imprima os resultados.
+let positivo: boolean = true;
+let negativo: boolean = false;
+
+console.log(`And: ${positivo && negativo}`);
+console.log(`Or: ${positivo || negativo}`);
+console.log(`Not: ${!positivo}`);
