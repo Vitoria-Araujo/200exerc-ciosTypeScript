af3b56669195f4b4b3908ea9f9af3357d16baa91
Vitoria-Araujo <vitoria.araujo1@hotmail.com git config --global user.name Vitoria-Araujogit statusgit config --global user.email vitoria.araujo1@hotmail.comgit config --global user.email vitoria.araujo1@hotmail.com>
2025-10-01 11:39:51 -0300

    Declare duas variáveis chamadas nome e sobrenome, atribua o seu nome e sobrenome a elas. Concatene-as em uma terceira variável.

---
+++ exercicio4.ts
@@ -0,0 +1,6 @@
+//Declare duas variáveis chamadas "nome" e "sobrenome", atribua o seu nome e sobrenome a elas. Concatene-as em uma terceira variável chamada "nomeCompleto" e imprima.
+let nome: string = 'Vitória ';
+let sobrenome:string = 'Araujo';
+let nomeCompleto: string = nome + sobrenome;
+
+console.log(`Seu nome completo é ${nomeCompleto}`);
